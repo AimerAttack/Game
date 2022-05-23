31c81202235c9dac0f9147e2ff44110188f84031
AimerAttack <wcpwcp9@gmail.com>
2022-05-23 16:23:43 +0800

    PolygonButton

--- Assets/GameFrame/Utility/UI/PolygonImage.cs
+++ Assets/GameFrame/Utility/UI/PolygonImage.cs
@@ -1,3 +1,4 @@
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace GameFrame.Utility.UI
@@ -5,6 +6,23 @@ namespace GameFrame.Utility.UI
     //异形响应区域图片
     public class PolygonImage : Image
     {
-        
+        private PolygonCollider2D _polygon;
+
+        private PolygonCollider2D Polygon
+        {
+            get
+            {
+                if (_polygon == null)
+                    _polygon = GetComponent<PolygonCollider2D>();
+
+                return _polygon;
+            }
+        }
+        public override bool IsRaycastLocationValid(Vector2 screenPoint, Camera eventCamera)
+        {
+            Vector3 point;
+            RectTransformUtility.ScreenPointToWorldPointInRectangle(rectTransform, screenPoint, eventCamera, out point);
+            return Polygon.OverlapPoint(point);
+        }
     }
 }
